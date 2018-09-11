d091f9e18da4eda6402384bef14aa5e4dc402aff
chargnn <alexis.410@hotmail.com>
2018-09-11 15:56:49 -0400

    Added navigation drawer instead of menu option

--- app/src/main/java/coulombe/twither/Home/HomeActivity.java
+++ app/src/main/java/coulombe/twither/Home/HomeActivity.java
@@ -2,10 +2,16 @@ package coulombe.twither.Home;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.ColorDrawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -29,6 +35,8 @@ import coulombe.twither.Profile.ProfileActivity;
 import coulombe.twither.R;
 
 public class HomeActivity extends AppCompatActivity {
+
+    ActionBarDrawerToggle toggleDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,67 +74,72 @@ public class HomeActivity extends AppCompatActivity {
                 openSeeMessageActivity();
             }
         });
+
+        final NavigationView drawer = findViewById(R.id.drawer);
+        final DrawerLayout drawer_layout = findViewById(R.id.drawer_layout);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toggleDrawer = new ActionBarDrawerToggle(this, drawer_layout, R.string.OpenDrawer, R.string.CloseDrawer);
+        drawer_layout.addDrawerListener(toggleDrawer);
+        toggleDrawer.syncState();
+
+        drawer.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem){
+                int id = menuItem.getItemId();
+
+                switch(id){
+                    case R.id.navigation_item_1:{
+                        ListView lv = findViewById(R.id.home_list_view);
+                        lv.smoothScrollToPosition(0);
+                        break;
+                    }
+                    case R.id.navigation_item_2:{
+                        openProfileActivity();
+                        break;
+                    }
+                    case R.id.navigation_sub_item_1:{
+                        break;
+                    }
+                    case R.id.navigation_sub_item_2:{
+                        AlertDialog alertDialog = new AlertDialog.Builder(HomeActivity.this).create();
+                        alertDialog.setTitle("À propos de Twither");
+                        alertDialog.setMessage(Html.fromHtml("<p>Fait par: <b>Alexis Coulombe</b></p>"));
+                        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                });
+                        alertDialog.show();
+                        break;
+                    }
+                }
+
+                drawer_layout.closeDrawers();
+                return false;
+            }
+        });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        MenuItem back2Top = menu.add(R.string.Home);
-        back2Top.setIcon(R.drawable.ic_home);
-        back2Top.getIcon().setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
-        back2Top.setShowAsAction(back2Top.SHOW_AS_ACTION_ALWAYS);
-        back2Top.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                ListView lv = findViewById(R.id.home_list_view);
-                lv.smoothScrollToPosition(0);
-                return true;
-            }
-        });
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(toggleDrawer.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-        MenuItem profile = menu.add(R.string.Profile);
-        profile.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                openProfileActivity();
-                return true;
-            }
-        });
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        toggleDrawer.syncState();
+    }
 
-        MenuItem about = menu.add(R.string.About);
-        about.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                AlertDialog alertDialog = new AlertDialog.Builder(HomeActivity.this).create();
-                alertDialog.setTitle("À propos de Twither");
-                alertDialog.setMessage(Html.fromHtml("<p>Fait par: <b>Alexis Coulombe</b></p>"));
-                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                            }
-                        });
-                alertDialog.show();
-                return true;
-            }
-        });
-
-        MenuItem quit = menu.add(R.string.Quit);
-        quit.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                finishAffinity();
-                return true;
-            }
-        });
-
-        MenuItem disconnect = menu.add(R.string.Disconnect);
-        disconnect.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                return true;
-            }
-        });
-        return true;
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        toggleDrawer.onConfigurationChanged(newConfig);
+        super.onConfigurationChanged(newConfig);
     }
 
     private void openSendMessageActivity(){
