83f0ee17ff7d8f2aac5fa26483e7cc3863b35d33
Chargnn <alexis.410@hotmail.com>
2018-09-24 20:37:30 -0400

    Added temporary login validation using mock

--- app/src/main/java/coulombe/twither/Login/LoginActivity.java
+++ app/src/main/java/coulombe/twither/Login/LoginActivity.java
@@ -3,15 +3,23 @@ package coulombe.twither.Login;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import coulombe.twither.Global.TwitUser;
 import coulombe.twither.Home.HomeActivity;
 import coulombe.twither.R;
+import coulombe.twither.Service.HttpService;
+import coulombe.twither.Service.TwitUser.IMockServiceUser;
 import coulombe.twither.Signup.SignupActivity;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -50,7 +58,38 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void openHomeActivity(){
-        Intent i = new Intent(this, HomeActivity.class);
-        startActivity(i);
+        final Intent i = new Intent(this, HomeActivity.class);
+        IMockServiceUser service = HttpService.getMockUser();
+        service.get().enqueue(new Callback<TwitUser>() {
+            @Override
+            public void onResponse(Call<TwitUser> call, Response<TwitUser> response) {
+                EditText username = findViewById(R.id.editText);
+                EditText password = findViewById(R.id.editText2);
+
+                if(username == null || username.getText().equals("")) {
+                    loginFailure();
+                    return;
+                }
+
+                if(password == null || password.getText().equals("")) {
+                    loginFailure();
+                    return;
+                }
+
+                if(response.body().nickname.toString().equals(username.getText().toString()) || response.body().email.toString().equals(username.getText().toString()) && response.body().password.toString().equals(password.getText().toString()))
+                    startActivity(i);
+                else
+                    loginFailure();
+            }
+
+            @Override
+            public void onFailure(Call<TwitUser> call, Throwable t) {
+                loginFailure();
+            }
+        });
+    }
+
+    private void loginFailure(){
+        Toast.makeText(LoginActivity.this, "Mauvais compte (temporaire)", Toast.LENGTH_SHORT).show();
     }
 }
